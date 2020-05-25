9ce7f81e9efada2fcba7060bc75c4dafb2daaf7d
fdolzanes1 <fdolzanes1@gmail.com>
2020-05-24 20:13:47 -0400

    Add controllers and validates

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'static_pages#index'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 end
